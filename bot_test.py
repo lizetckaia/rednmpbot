f8d311b7c6e4fa086c89da2b6535a863b8e1e248
eletc <eletckaia2002@gmail.com>
2021-12-18 17:26:56 +0300

    .

--- bot_test.py
+++ bot_test.py
@@ -1,10 +1,17 @@
 import unittest
-from bot import generate_keyboard
+from bot import generate_keyboard, start, unknown, info
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class botTest(unittest.TestCase):
+    def test_keyboard(self):
+        self.assertNotEqual(generate_keyboard(), self.keyboard)
+
+    def test_unknown(self):
+        self.assertEqual(unknown('/help'), "Я даже не знаю, что и ответить... Может /info?")
+
+    def test_info(self):
+        self.assertEqual(info('/info'), 'Я отправляю самые ожидаемые анонсы из мира кино, сериалов и аниме - выбери, '
+                                        'что тебя интересует.А ещё можешь подписаться на паблик, нажав кнопку nmp 🥺👉👈')
 
 
 if __name__ == '__main__':
