22e537c1445393fa32117ab217da1715dc1d415d
eletc <eletckaia2002@gmail.com>
2021-12-18 20:48:56 +0300

    Initial commit

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
 import unittest
 from unittest.mock import MagicMock
-from bot import info, unknown
-
+from bot import info, unknown, start
 
 
 async def async_magic():
@@ -18,19 +17,31 @@ class MyTestCase(unittest.TestCase):
         effective_chat = MagicMock(id=123)
         update = MagicMock(effective_chat=effective_chat)
         info(update=update, context=context)
-        bot.send_message.assert_called_with(chat_id=123, text = 'Я отправляю самые ожидаемые анонсы из мира кино, '
-                                                                'сериалов и аниме - выбери, что тебя интересует. '
-                                                                'А ещё можешь подписаться на паблик, '
-                                                                'нажав кнопку nmp 🥺👉👈')
+        bot.send_message.assert_called_with(chat_id=123, text='Я отправляю самые ожидаемые анонсы из мира кино, '
+                                                              'сериалов и аниме - выбери, что тебя интересует. '
+                                                              'А ещё можешь подписаться на паблик, '
+                                                              'нажав кнопку nmp 🥺👉👈')
+
     def test_unknown(self):
         bot = MagicMock()
         context = MagicMock(bot=bot)
         effective_chat = MagicMock(id=123)
         update = MagicMock(effective_chat=effective_chat)
-        info(update=update, context=context)
+        unknown(update=update, context=context)
         bot.send_message.assert_called_with(chat_id=123, text="Я даже не знаю, что и ответить... Может /info?")
 
-
+    def test_start(self):
+        bot = MagicMock()
+        context = MagicMock(bot=bot)
+        effective_chat = MagicMock(id=123)
+        update = MagicMock(effective_chat=effective_chat)
+        start(update=update, context=context)
+        bot.send_message.assert_called_with(chat_id=123, text=f'Привет, {user_name}! Это бот группы no more people. '
+                                                              f'Для того чтобы узнать, что я умею, нажми /info или '
+                                                              f'сразу выбери то, что тебе нужно. ')
+        bot.send_photo.assert_called_with(chat_id=123, photo='https://sun9-38.userapi.com/impg/-krE1PvuFBhYGOydVcVm4jGBcf'
+                                                             '_owY17SzWljw/aVTJnS6cGE4.jpg?size=1920x1038&quality=96&si'
+                                                             'gn=73fc4f99180a381495d0c55a43945bf3&type=album'
 
 
 if __name__ == '__main__':
